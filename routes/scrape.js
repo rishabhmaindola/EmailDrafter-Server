9b4b4c2604a90b7bf1828d6876c190dee01bf9e7
Rishabh Maindola <143874827+rishabhmaindola@users.noreply.github.com>
2024-04-07 13:01:46 +0530

    Update scrape.js

--- routes/scrape.js
+++ routes/scrape.js
@@ -3,14 +3,14 @@ const router = express.Router();
 const scrapeWebsite = require('../scrapers/websiteScraper');
 const scrapeSitemap = require('../scrapers/aboutPageScraper');
 
-router.post('/info',  async (req, res) => {
+router.post('/info', async (req, res) => {
     const { websiteUrl, aboutPageUrl } = req.body;
     console.log(websiteUrl, aboutPageUrl);
     try {
-        const websiteInfo = await scrapeWebsite(websiteUrl);
-        const aboutPageInfo = await scrapeSitemap(aboutPageUrl);
-        console.log(websiteInfo);
-        console.log(aboutPageInfo);
+        const [websiteInfo, aboutPageInfo] = await Promise.all([
+            scrapeWebsite(websiteUrl),
+            scrapeSitemap(aboutPageUrl)
+        ]);
         if (websiteInfo && aboutPageInfo) {
             res.status(200).json({ website: websiteInfo, about: aboutPageInfo });
         } else {
